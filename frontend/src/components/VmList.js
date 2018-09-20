9cdf396f80282d06095abe3467433311306730b7
Denis Bazhenov <bazhenov@farpost.com>
2018-09-21 00:06:11 +1000

    refactoring

--- frontend/src/components/VmList.js
+++ frontend/src/components/VmList.js
@@ -6,23 +6,18 @@ export class VmList extends React.Component {
     onVmRun: null
   };
 
+  createListActions(vm) {
+    return [<Button type="primary" icon="caret-right" shape="circle" onClick={() => this.props.onVmRun(vm)} />]
+  }
+
   render() {
     return (
       <List
         dataSource={this.props.vms}
-        renderItem={i => (
-          <List.Item
-            actions={[
-              <Button
-                type="primary"
-                icon="caret-right"
-                shape="circle"
-                onClick={() => this.props.onVmRun(i)}
-              />
-            ]}
-          >
+        renderItem={vm => (
+          <List.Item actions={this.createListActions(vm)}>
             <Skeleton title={false} loading={false} active>
-              <p className="vm-name">{i.name}</p>
+              <p className="vm-name">{vm.name}</p>
             </Skeleton>
           </List.Item>
         )}
