6c940f4fd9deaded7552245032e99bc61a31e35e
Denis Bazhenov <bazhenov@farpost.com>
2018-09-21 10:58:38 +1000

    reqwest library included

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { VmList } from "./VmList";
+import reqwest from 'reqwest'
 
 export class App extends React.Component {
   render() {
     return (
       <VmList
         vms={[{ id: 0, name: "Vm 1" }, { id: 1, name: "Vm 2" }]}
-        onVmRun={id => alert(JSON.stringify(id))}/>
+        onVmRun={this.vmStart}/>
     );
+  }
+
+  vmStart(vm) {
+    return reqwest({url: "/api/vm/start", method: "POST", data: {uuid: vm.UUID}})
+  }
+
+  vmStop(vm) {
+    return reqwest({url: "/api/vm/stop", method: "POST", data: {uuid: vm.UUID}})
+  }
+
+  vmList() {
+    return reqwest({url: "/api/vm/list", method: "GET"})
   }
 }
